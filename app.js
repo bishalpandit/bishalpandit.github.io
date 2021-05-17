c71c3d428b3ebc05bbc856776fd59823bc1ea4f0
ashikishere <azashikishere@gmaol.com>
2021-05-18 00:37:31 +0600

    chnages title and nav

--- app.js
+++ app.js
@@ -1,18 +1,28 @@
-if ($('.text-slider').length == 1) {
-    var typed_strings = $('.text-slider-items').text();
-    var typed = new Typed('.text-slider', {
-      strings: typed_strings.split(','),
-      typeSpeed: 80,
-      loop: true,
-      backDelay: 1100,
-      backSpeed: 30
-    });
-  }
+if ($(".text-slider").length == 1) {
+  var typed_strings = $(".text-slider-items").text();
+  var typed = new Typed(".text-slider", {
+    strings: typed_strings.split(","),
+    typeSpeed: 80,
+    loop: true,
+    backDelay: 1100,
+    backSpeed: 30,
+  });
+}
 
-  $(window).on('load', function() {
-    if ($('#preloader').length) {
-      $('#preloader').delay(200).fadeOut('slow', function() {
+$(window).on("load", function () {
+  if ($("#preloader").length) {
+    $("#preloader")
+      .delay(200)
+      .fadeOut("slow", function () {
         $(this).remove();
       });
-    }
-  });
+  }
+});
+
+//fixed header
+
+window.addEventListener("scroll", function () {
+  let header = document.querySelector("nav");
+
+  header.classList.toggle("scrolling-active", window.scrollY > 0);
+});
