e710ec890520bf5340eacc23e6f12f54c74c7fa7
bishalpandit <bishalpandit17@gmail.com>
2021-05-25 13:01:44 +0530

    finished adding animations

--- app.js
+++ app.js
@@ -16,3 +16,55 @@ if ($('.text-slider').length == 1) {
       });
     }
   });
+
+
+  window.addEventListener("scroll", function () {
+    let header = document.querySelector("nav");
+  
+    header.classList.toggle("scrolling-active", window.scrollY > 0);
+  });
+  
+
+  $(document).ready(
+  
+    function() {
+  
+  $(' .about-section').waypoint( function(direction) {
+  
+        $(' .about-section').addClass('animated fadeIn');
+  
+    }, {
+  
+        offset : '65%'
+    });
+
+  
+  $(' .tc1').waypoint(function(direction){
+      $(' .tc1').addClass('animated fadeInLeft')
+  }, {
+  
+      offset : '60%'
+  });
+  $(' .tc2').waypoint(function(direction){
+      $(' .tc2').addClass('animated fadeInRight')
+  }, {
+  
+      offset : '60%'
+  });
+  $(' .tc3').waypoint(function(direction){
+      $(' .tc3').addClass('animated fadeInLeft')
+  }, {
+  
+      offset : '60%'
+  });
+  $(' .tc4').waypoint(function(direction){
+      $(' .tc4').addClass('animated fadeInRight')
+  }, {
+  
+      offset : '60%'
+  });
+  
+
+    
+  
+  });
